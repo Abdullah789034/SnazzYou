71d17217ad4aac9d97d1cf74d3e80a2f691d1a9e
Muhammad Abdullah <95620935+Abdullah789034@users.noreply.github.com>
2024-05-22 01:41:17 +0500

    Modal Updated

--- src/components/CommonComponents/contactFormModal.jsx
+++ src/components/CommonComponents/contactFormModal.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import CrossCircledIcon from "../../assets/icons/cross-circled-Icon";
 import CCModal from "./CCModal";
+import {
+  ModalBody,
+  ModalFooter,
+  Input,
+  Button,
+} from "@nextui-org/react";
 
 const ContactFormModal = ({ isOpen, onClose, title = "Book A Call" }) => {
   return (
@@ -11,7 +17,44 @@ const ContactFormModal = ({ isOpen, onClose, title = "Book A Call" }) => {
           <CrossCircledIcon />
         </div>
       </div>
-      <div>modal content</div>
+      <div>
+        <ModalBody>
+          <Input
+            autoFocus
+            placeholder="First Name"
+            variant="bordered"
+            className="mb-4"
+          />
+          <Input
+            placeholder="Last Name"
+            variant="bordered"
+            className="mb-4"
+          />
+          <Input
+            type="email"
+            placeholder="Email"
+            variant="bordered"
+            className="mb-4"
+          />
+          <Input
+            placeholder="Phone"
+            variant="bordered"
+            className="mb-4"
+          />
+          <textarea
+            placeholder="Your Message"
+            className="w-full p-2 border border-gray-300 bg-white rounded"
+            rows="6"
+            required
+          />
+        </ModalBody>
+        <ModalFooter>
+
+          <Button className="w-full bg-[#fff04f] border-black" onPress={onClose}>
+            Submit
+          </Button>
+        </ModalFooter>
+      </div>
     </CCModal>
   );
 };
