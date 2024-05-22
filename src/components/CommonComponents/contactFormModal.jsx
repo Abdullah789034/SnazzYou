582c4eea41f215ec1fc75299c1bd4fc2ed8eafa5
asadahmad27 <amasadmuhgal@gmail.com>
2024-05-22 10:35:56 +0500

    page done

--- src/components/CommonComponents/contactFormModal.jsx
+++ src/components/CommonComponents/contactFormModal.jsx
@@ -1,14 +1,52 @@
 import React from "react";
 import CrossCircledIcon from "../../assets/icons/cross-circled-Icon";
 import CCModal from "./CCModal";
-import {
-  ModalBody,
-  ModalFooter,
-  Input,
-  Button,
-} from "@nextui-org/react";
+import { ModalBody, ModalFooter, Input, Button } from "@nextui-org/react";
 
 const ContactFormModal = ({ isOpen, onClose, title = "Book A Call" }) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("http://146.190.124.167:7878/mailto", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (response.ok) {
+      alert("Message sent successfully");
+      // Reset form fields
+      setFormData({
+        name: "",
+        lastName: "",
+        email: "",
+        phone: "",
+        message: "",
+      });
+      onClose();
+    } else {
+      alert("Failed to send message");
+    }
+  };
+
   return (
     <CCModal isOpen={isOpen} bodyClass="py-6">
       <div className="flex items-center pb-4">
@@ -18,42 +56,59 @@ const ContactFormModal = ({ isOpen, onClose, title = "Book A Call" }) => {
         </div>
       </div>
       <div>
-        <ModalBody>
-          <Input
-            autoFocus
-            placeholder="First Name"
-            variant="bordered"
-            className="mb-4"
-          />
-          <Input
-            placeholder="Last Name"
-            variant="bordered"
-            className="mb-4"
-          />
-          <Input
-            type="email"
-            placeholder="Email"
-            variant="bordered"
-            className="mb-4"
-          />
-          <Input
-            placeholder="Phone"
-            variant="bordered"
-            className="mb-4"
-          />
-          <textarea
-            placeholder="Your Message"
-            className="w-full p-2 border border-gray-300 bg-white rounded"
-            rows="6"
-            required
-          />
-        </ModalBody>
-        <ModalFooter>
+        <Input
+          autoFocus
+          placeholder="First Name"
+          variant="bordered"
+          className="mb-4"
+          name="name"
+          required
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <Input
+          placeholder="Last Name"
+          variant="bordered"
+          className="mb-4"
+          required
+          name="lastName"
+          value={formData.lastName}
+          onChange={handleChange}
+        />
+        <Input
+          type="email"
+          placeholder="Email"
+          variant="bordered"
+          className="mb-4"
+          name="email"
+          required
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <Input
+          placeholder="Phone"
+          variant="bordered"
+          className="mb-4"
+          name="phone"
+          value={formData.phone}
+          onChange={handleChange}
+        />
+        <textarea
+          placeholder="Your Message"
+          className="w-full p-2 border border-gray-300 bg-white rounded"
+          rows="6"
+          name="message"
+          required
+          value={formData.message}
+          onChange={handleChange}
+        />
 
-          <Button className="w-full bg-[#fff04f] border-black" onPress={onClose}>
-            Submit
-          </Button>
-        </ModalFooter>
+        <Button
+          className="w-full bg-[#fff04f] border-black"
+          onPress={handleSubmit}
+        >
+          Submit
+        </Button>
       </div>
     </CCModal>
   );
--- src/components/MainComponents/LandingPage/BookCall.jsx
+++ src/components/MainComponents/LandingPage/BookCall.jsx
@@ -1,63 +1,64 @@
-import React, { useState } from 'react';
-import callImg from '../../../assets/images/Book-a-Call.png';
-import ContactFormModal from '../../CommonComponents/contactFormModal';
+import React, { useState } from "react";
+import callImg from "../../../assets/images/Book-a-Call.png";
+import ContactFormModal from "../../CommonComponents/contactFormModal";
 
 const BookCall = () => {
-    const [isContactFormOpen, setIsContactFormOpen] = useState(false);
+  const [isContactFormOpen, setIsContactFormOpen] = useState(false);
 
-    const handleBookCallClick = () => {
-        setIsContactFormOpen(true);
-    };
+  const handleBookCallClick = () => {
+    setIsContactFormOpen(true);
+  };
 
-    return (
-        <>
-            <div className='w-full lg:h-[550px] bg-black'>
-                <div className='flex flex-wrap'>
-                    <div className='w-full lg:w-1/2 flex items-center justify-center p-4'>
-                        <div className="flex flex-col justify-center w-full lg:w-3/4 h-auto p-4 space-y-4 text-white">
-                            {/* Heading */}
-                            <div className="w-full">
-                                <h2 className="text-4xl font-bold font-['Montserrat']">What are you waiting for?</h2>
-                            </div>
+  return (
+    <>
+      <div className="w-full bg-black">
+        <div className="flex flex-wrap">
+          <div className="w-full lg:w-1/2 flex items-center justify-center p-4">
+            <div className="flex flex-col justify-center w-full lg:w-3/4 h-auto p-4 space-y-4 text-white">
+              {/* Heading */}
+              <div className="w-full">
+                <h2 className="text-4xl font-bold font-['Montserrat']">
+                  What are you waiting for?
+                </h2>
+              </div>
 
-                            {/* Paragraphs */}
-                            <div className="w-full space-y-4 text-xl">
-                                <p>
-                                    At Capitals Club, we're on the lookout for serious
-                                    investors with ambitious financial goals, creating lasting
-                                    wealth and prosperity.
-                                </p>
-                                <p>
-                                    Join us and gain access to expert guidance, innovative
-                                    strategies, and a supportive community dedicated to your
-                                    financial success.
-                                </p>
-                                <p>
-                                    With Capitals Club, you'll not only stay ahead of the
-                                    curve but also pave the way for a brighter financial
-                                    future.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='w-full flex items-center justify-center p-4'>
-                        <img
-                            src={callImg}
-                            alt="Book a Call"
-                            className='w-2/4 lg:w-1/4 h-auto cursor-pointer'
-                            onClick={handleBookCallClick}
-                        />
-                    </div>
-                </div>
+              {/* Paragraphs */}
+              <div className="w-full space-y-4 text-xl">
+                <p>
+                  At Capitals Club, we're on the lookout for serious investors
+                  with ambitious financial goals, creating lasting wealth and
+                  prosperity.
+                </p>
+                <p>
+                  Join us and gain access to expert guidance, innovative
+                  strategies, and a supportive community dedicated to your
+                  financial success.
+                </p>
+                <p>
+                  With Capitals Club, you'll not only stay ahead of the curve
+                  but also pave the way for a brighter financial future.
+                </p>
+              </div>
             </div>
-
-            <ContactFormModal
-                isOpen={isContactFormOpen}
-                onClose={() => setIsContactFormOpen(false)}
-                title="Book A Call"
+          </div>
+          <div className="w-full flex items-center justify-center p-4">
+            <img
+              src={callImg}
+              alt="Book a Call"
+              className="w-2/4 lg:w-1/4 h-auto cursor-pointer"
+              onClick={handleBookCallClick}
             />
-        </>
-    );
-}
+          </div>
+        </div>
+      </div>
+
+      <ContactFormModal
+        isOpen={isContactFormOpen}
+        onClose={() => setIsContactFormOpen(false)}
+        title="Book A Call"
+      />
+    </>
+  );
+};
 
 export default BookCall;
