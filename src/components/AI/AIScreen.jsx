7988c9eeb0f3b095bd5c927a5ba16a17a7263309
Muhammad Abdullah <95620935+Abdullah789034@users.noreply.github.com>
2024-06-28 01:46:24 +0500

    Added Page

---
+++ src/components/AI/AIScreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AIScreen = () => {
+    return (
+        <div className='flex flex-col items-center justify-center h-screen w-screen'>
+            <div>
+                <p className='text-2xl'>Details about User’s Image that AI gathered</p>
+            </div>
+        </div>
+    )
+}
+
+export default AIScreen
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,14 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Views/home";
+import Analyzing from "./components/LandingPage/Analyzing";
+import AIScreen from "./components/AI/AIScreen";
 
 const AppRoutes = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/analyze" element={<Analyzing />} />
+        <Route path="/ai" element={<AIScreen />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 };
 
